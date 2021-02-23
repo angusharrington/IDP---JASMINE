8447d27e862f9d6ba154c5db7e96b87f0eb986bc
Oscar <osaharoy@gmail.com>
2021-02-23 19:14:15 +0000

    updated controller, robot can move around and pick up blocks but gets stuck

--- Mechanics/Webots_Model/controllers/JASMINE_controller/JASMINE_controller.py
+++ Mechanics/Webots_Model/controllers/JASMINE_controller/JASMINE_controller.py
@@ -2,43 +2,71 @@
 # Johns Automated Sorter for Meaningless Inconsequential Non-existent Experiments
 
 import math
-from controller import Robot, Motor, DistanceSensor
+from controller import Robot, Motor, DistanceSensor, GPS
+
+# dot product
+dot = lambda v1, v2 : v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+
+# magnitude of vector
+mod = lambda v : ( v[0]**2 + v[1]**2 + v[2]**2 ) ** 0.5
 
 robot = Robot()
 
-# get the time step of the current world.
+gps = GPS("gps")
+
+# get the time step in milliseconds of the current world
 timestep = int( robot.getBasicTimeStep() )
+
+# dt in timestep in seconds 
+dt = timestep / 1000
 
 # get the motors and distance sensors
 leftWheelMotor           = robot.getDevice( "LeftWheelMotor"           )
 rightWheelMotor          = robot.getDevice( "RightWheelMotor"          )
+clawMotor                = robot.getDevice( "ClawMotor"                )
 frontLeftDistanceSensor  = robot.getDevice( "DistanceSensorFrontLeft"  )
 frontRightDistanceSensor = robot.getDevice( "DistanceSensorFrontRight" )
 
-# enable the distance sensors
-frontLeftDistanceSensor.enable(  100 )
-frontRightDistanceSensor.enable( 100 )
+# start the GPS
+gps.enable( timestep )
 
-# set the motors positions so that we can set the velocity
-leftWheelMotor.setPosition(  100000 )
-rightWheelMotor.setPosition( 100000 )
+# enable the distance sensors
+frontLeftDistanceSensor.enable(  timestep )
+frontRightDistanceSensor.enable( timestep )
+
+# set the motors positions to infinity so we control only the velocity
+leftWheelMotor.setPosition(  float('inf') )
+rightWheelMotor.setPosition( float('inf') )
+clawMotor.setPosition( -0.1 )
+
+clawAngle = -0.1
+prevPos   = gps.getValues()
 
 # Main loop
 while robot.step(timestep) != -1:
 
-    leftDist = frontLeftDistanceSensor.getValue()
+    pos     = gps.getValues()
+    x, y, z = pos
 
-    # sometimes we get NaN so ignore
-    if math.isnan(leftDist):
-        continue
+    vel = [ (a-b)/dt for a,b in zip(pos, prevPos) ]
+    prevPos = pos
 
-    if leftDist >= 999:
+    #print( " ".join( ["%.2f" % v for v in pos] ) )
+    print( " ".join( ["%.2f" % v for v in vel] ) )
 
-        leftWheelMotor.setVelocity(  5 )
-        rightWheelMotor.setVelocity( 5 )
+    nearWall         = abs(x) > 0.8 or abs(z) > 0.8
+    goingTowardsWall = dot( pos, vel ) / mod(vel) > -0.2
+    shouldTurn       = nearWall and goingTowardsWall
 
-    else:
+    blockOnLeft  = frontLeftDistanceSensor.getValue()  < 700
+    blockOnRight = frontRightDistanceSensor.getValue() < 700
 
-        print("wall!")
+    leftWheelMotor.setVelocity(  20 )
+    rightWheelMotor.setVelocity( 20 - shouldTurn * 15 )
 
-        rightWheelMotor.setVelocity( 0 )
+    if (blockOnLeft or blockOnRight) and not shouldTurn:
+        clawAngle += 6
+
+    clawAngle = ( clawAngle + 0.1 ) * 0.7 - 0.1
+
+    clawMotor.setPosition( clawAngle )
