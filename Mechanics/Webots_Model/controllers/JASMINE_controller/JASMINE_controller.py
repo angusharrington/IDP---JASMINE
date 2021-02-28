605f4446ec01e4383ff0cc060e8c568c5f10b5ac
Oscar Saharoy <osaharoy@gmail.com>
2021-02-28 13:54:11 +0000

    put jasmine controller into a class

--- Mechanics/Webots_Model/controllers/JASMINE_controller/JASMINE_controller.py
+++ Mechanics/Webots_Model/controllers/JASMINE_controller/JASMINE_controller.py
@@ -1,121 +1,76 @@
+
 # idp group L203
 # Johns Automated Sorter for Meaningless Inconsequential Non-existent Experiments
 
 import math
+import numpy as np
 from controller import Robot, Motor, DistanceSensor, GPS
 
-# magnitude of vector
-mod = lambda v : ( v[0]**2 + v[1]**2 + v[2]**2 ) ** 0.5
+# Jasmine class inherits from Robot class
+# so we can use eg self.step instead of robot.step
+class Jasmine(Robot):
 
-# dot product
-dot = lambda v1, v2 : v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+    def __init__(self):
 
-# dot product but normalise first
-normDot = lambda v1, v2 : ( v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] ) / ( mod(v1) * mod(v2) )
+        # initialise the underlying Robot
+        Robot.__init__(self)
 
-# subtract 2 vectors
-subv = lambda v1, v2 : [ v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2] ]
+        # get the world's timestep and initialise the sensors
+        self.timestep = int( self.getBasicTimeStep() )
+        self.initialiseSensors()
 
-# origin and red square locations
-origin    = [0, 0, 0]
-redSquare = [0, 0, 0.4]
+        # variables that help the robot work
+        self.prevPos      = self.position
+        self.clawAngle    = -0.1
+        self.gettingBlock = False
+        self.haveBlock    = False
 
-# get robot and GPS
-robot = Robot()
-gps = GPS("gps")
+        # enter the main loop
+        self.mainLoop()
 
-# get the time step in milliseconds of the current world
-timestep = int( robot.getBasicTimeStep() )
 
-# dt in timestep in seconds 
-dt = timestep / 1000
+    def initialiseSensors(self):
 
-# get the motors and distance sensors
-leftWheelMotor           = robot.getDevice( "LeftWheelMotor"           )
-rightWheelMotor          = robot.getDevice( "RightWheelMotor"          )
-clawMotor                = robot.getDevice( "ClawMotor"                )
-frontLeftDistanceSensor  = robot.getDevice( "DistanceSensorFrontLeft"  )
-frontRightDistanceSensor = robot.getDevice( "DistanceSensorFrontRight" )
+        # get all the robot's sensors
+        self.gps             = GPS("gps")
+        self.leftWheelMotor  = self.getDevice( "LeftWheelMotor"      )
+        self.rightWheelMotor = self.getDevice( "RightWheelMotor"     )
+        self.clawMotor       = self.getDevice( "ClawMotor"           )
+        self.distanceSensor  = self.getDevice( "DistanceSensorFront" )
 
-# start the GPS
-gps.enable( timestep )
+        # enable the sensors
+        self.gps.enable( self.timestep )
+        self.distanceSensor.enable( self.timestep )
 
-# enable the distance sensors
-frontLeftDistanceSensor.enable(  timestep )
-frontRightDistanceSensor.enable( timestep )
+        # set the motors' positions to infinity so we can use velocity control
+        self.leftWheelMotor.setPosition(  float('inf') )
+        self.rightWheelMotor.setPosition( float('inf') )
 
-# set the motors positions to infinity so we control only the velocity
-leftWheelMotor.setPosition(  float('inf') )
-rightWheelMotor.setPosition( float('inf') )
-clawMotor.setPosition( -0.1 )
+        # claw is using position control but needs to be a bit down so we can see the blocks
+        self.clawMotor.setPosition( -0.1 )
 
-# variables that help the robot work
-clawAngle    = -0.1
-prevPos      = gps.getValues()
-gettingBlock = False
-haveBlock    = False
 
-# Main loop
-while robot.step(timestep) != -1:
+    @property
+    def position(self):
+        
+        return np.array( self.gps.getValues() )
 
-    # get the position from the gps
-    pos     = gps.getValues()
-    x, y, z = pos
 
-    # calculate velocity with single sided difference approximation
-    vel = [ (a-b)/dt for a,b in zip(pos, prevPos) ]
-    prevPos = pos
+    def mainLoop(self):
 
-    # can print gps position or velocity
-    print( "pos: " + " ".join( ["%.2f" % v for v in pos] ) )
-    print( "vel: " + " ".join( ["%.2f" % v for v in vel] ) )
+        # loop until simulation end
+        while self.step( self.timestep ) != -1:
 
-    if haveBlock:
+            # get the position from the gps
+            x, y, z = pos = self.position
+            dt = self.timestep / 1000
 
-        # get vector to red square
-        toSquare = subv( redSquare, pos )
+            # calculate velocity with single sided difference approximation
+            vel = [ (a-b)/dt for a,b in zip(pos, self.prevPos) ]
+            self.prevPos = pos
 
-        # decide if we should turn
-        shouldTurn = normDot( toSquare, vel ) < 0.9
+            # can print gps position or velocity
+            print( "pos: " + " ".join( ["%.2f" % v for v in pos] ) )
+            print( "vel: " + " ".join( ["%.2f" % v for v in vel] ) )
 
-        # turn to point at the square and drive
-        leftWheelMotor.setVelocity(  20 - shouldTurn * 11 )
-        rightWheelMotor.setVelocity( 20 - shouldTurn * 17 )
-
-        # we are at the square if the distance to it is small
-        atSquare = mod(toSquare) < 0.1
-
-        if atSquare:
-
-            # stop the motors and exit
-            leftWheelMotor.setVelocity(  0 )
-            rightWheelMotor.setVelocity( 0 )
-            break
-
-    else:
-
-        # decide if we are about to hit a wall
-        nearWall         = abs(x) > 0.8 or abs(z) > 0.8
-        goingTowardsWall = normDot( pos, vel ) > 0.1
-        shouldTurn       = nearWall and goingTowardsWall
-
-        # find if we have a block on the left or right
-        blockOnLeft  = frontLeftDistanceSensor.getValue()  < 700
-        blockOnRight = frontRightDistanceSensor.getValue() < 700
-
-        # set motors to turn if neccesary
-        leftWheelMotor.setVelocity(  20 )
-        rightWheelMotor.setVelocity( 20 - shouldTurn * 15 )
-
-        # actuate claw to grab a block
-        if (blockOnLeft or blockOnRight) and not shouldTurn:
-            
-            clawAngle   += 6
-            gettingBlock = True
-
-    # exponentially decay claw angle
-    clawAngle = ( clawAngle + 0.1 ) * 0.7 - 0.1
-    clawMotor.setPosition( clawAngle )
-
-    # we have captured the block when the claw has come down mostly
-    haveBlock = gettingBlock and clawAngle < 0
+Jasmine()
